8600101aea4ef234761df7200ca724b6b9bfc2ff
Shiv6821 <deshmukhshivam121@gmail.com>
2025-03-19 09:21:54 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.crs4w0c40aj6.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Shiv1234',
+    DB_DATABASE : 'webappdb'
 });
